f2fd97a07b7a782adf6f8afd6e3b377c505066d9
Eleni <elemandana@gmail.com>
2021-12-20 15:46:48 +0200

    commit

--- Server.java
+++ Server.java
@@ -6,10 +6,13 @@ public class Server {
     public static void main(String[] args) {
         try {
             int port_number = Integer.parseInt(args[0]);
+            Functions_server stub = new Functions_server();
 
             Registry rmiRegistry = LocateRegistry.createRegistry(port_number);
-            Funcs stub = (Funcs) rmiRegistry.lookup("functions");
+        
             rmiRegistry.rebind("functions", stub);
+
+            System.out.println("I work!");
 
         } catch (Exception e) {
             e.printStackTrace();
